e57a1167178f69043a14f86fdc02399932608933
Garcia Fabien <fab0670312047@gmail.com>
2022-11-27 11:40:48 +0100

    Introduce taill wind css

--- config.rb
+++ config.rb
@@ -78,6 +78,11 @@ set :haml, { ugly: true }
 # set :js_dir, 'javascripts'
 
 # set :images_dir, 'images'
+activate :external_pipeline,
+  name: :tailwind,
+  command: "npx tailwindcss -i ./source/stylesheets/site.css -o ./dist/stylesheets/site.css #{"--watch" unless build?}",
+  latency: 2,
+  source: "./dist/"
 
 activate :alias
 # Build-specific configuration
